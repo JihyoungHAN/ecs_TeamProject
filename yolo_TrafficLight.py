6cee5f3f6f227c4daf20ee84b46aea3cc049b8f0
JiHyoungHAN <gkswlgud4343@hufs.ac.kr>
2023-09-10 14:52:37 -0700

    Integration of image processing code and yolo code, and video processing

--- yolo_TrafficLight.py
+++ yolo_TrafficLight.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[1]:
 
 
 import cv2
 import numpy as np
+import math
 
 
-# In[12]:
+# In[2]:
 
 
 #loading YOLO
@@ -21,73 +22,85 @@ output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
 
-# In[6]:
+# In[3]:
 
 
 #bringing the image
-img = cv2.imread("trafficLight.jpg")
-img = cv2.resize(img, None, fx=0.4, fy=0.4)
-height, width, channels = img.shape
+video_capture = cv2.VideoCapture('traffic_light_video.mp4')
+#frame_interval =1000
+#frame_count = 0 
 
+while video_capture.isOpened():
+    ret, img = video_capture.read()
+    if not ret: 
+        break 
+        
+    img = cv2.resize(img, None, fx=0.4, fy=0.4)
+    height, width, channels = img.shape
+    
+    #Detecting Objects 
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5 and class_id == 9: 
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                # co-ordinate
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+                #extract traffic light image
+                img_trim = img[y:y+h, x:x+w]
+                cv2.imwrite('traffic_light_trim.jpg', img_trim)
+                org_img = cv2.imread('traffic_light_trim.jpg')
+                org_img = cv2.resize(org_img, dsize = (200, 400))
+                break
+                
+    
+    # changing the image color 
+    img = cv2.imread("traffic_light_trim.jpg", 1)
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# In[13]:
+    # Appling Bilateral filter  
+    sigma_color = 10.0
+    sigma_space = 10.0
+    filtered_image = cv2.bilateralFilter(gray_img, -1, sigma_color, sigma_space)
 
+    # Detect circle 
+    circles = cv2.HoughCircles(filtered_image, cv2.HOUGH_GRADIENT, 2, 30, param1=200, param2=50, minRadius=10, maxRadius=50)
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
 
-#Detecting Objects 
-blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-net.setInput(blob)
-outs = net.forward(output_layers)
+        for i in circles[0, :]:
+            center = (i[0], i[1])
+            radius = i[2]
+            k = i[0]
+            j = i[1]
+            B = float(img[j, k, 0])
+            G = float(img[j, k, 1])
+            R = float(img[j, k, 2])
+        
+            if R - B > 10 and R - G > 10 and R > 100:
+                print("Stop due to the RedLight")
+            if G - R > 10 and G - B > 10 and G > 100:
+                print("Detected GreenLight")
 
-
-# In[14]:
-
-
-class_ids = []
-confidences = []
-boxes = []
-for out in outs:
-    for detection in out:
-        scores = detection[5:]
-        class_id = np.argmax(scores)
-        confidence = scores[class_id]
-        if confidence > 0.5:
-            # Object detected
-            center_x = int(detection[0] * width)
-            center_y = int(detection[1] * height)
-            w = int(detection[2] * width)
-            h = int(detection[3] * height)
-            # 좌표
-            x = int(center_x - w / 2)
-            y = int(center_y - h / 2)
-            boxes.append([x, y, w, h])
-            confidences.append(float(confidence))
-            class_ids.append(class_id)
-
-
-# In[15]:
-
-
-indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-
-
-# In[ ]:
-
-
-font = cv2.FONT_HERSHEY_PLAIN
-for i in range(len(boxes)):
-    if i in indexes:
-        x, y, w, h = boxes[i]
-        label = str(classes[class_ids[i]])
-        color = colors[i]
-        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-        cv2.putText(img, label, (x, y + 30), font, 3, color, 3)
-cv2.imshow("Image", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
-# In[ ]:
-
+    #if cv2.waitKey(frame_interval) & 0xFF == ord('q'):
+        #break
 
 
 
